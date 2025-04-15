55c5158095f02acd2d6bc572854fad83d1e1095a
joaomrc <joaomariarc@gmail.com>
2025-04-15 18:36:33 +0100

    correção de erros na porta do backend do REST e CORS configurado

--- transporte_mercadorias_server.js
+++ transporte_mercadorias_server.js
@@ -8,6 +8,13 @@ const protoLoader = require('@grpc/proto-loader');
 const app = express();
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 const databaseFile = 'mercadorias.json';
 
 function loadData() {
